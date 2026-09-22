a91a40c302b6845547d103eb023c728eef3cda22
Газгиреев Хамза А <student106@ithub.local>
2026-09-22 11:46:49 +0300

    Добавлен маршрутизатор Express

--- server.js
+++ server.js
@@ -1,14 +1,34 @@
-const http = require('http')
-const express = require('express')
-const app = express()
+const express = require('express')// подключили библиотеку Express
+const app = express()// запустили Express 
 
-const PORT = 3000;
+const PORT = 3000; // Обозначили порт для дальнейшего запуска
 
-app.get('/' , (req , res)=>{
-    res.send("Главная страница")
+app.get('/' , (req , res)=>{ // создаем get запрос для получения главной страницы
+    res.send("Главная страница")// отправляем клиенту ответ respons
 })
 
-app.listen(PORT , ()=>{
+app.get('/about' , (req , res)=>{
+    res.send("О нас")
+})
+
+app.get('/contact' , (req , res)=>{
+    res.send("наш контакт:8(888)888 88-88")
+})
+
+app.get('/product/:id' , (req, res)=>{
+    console.log(req.params);
+    res.send(`Товар с id: ${req.params.id}`)
+    
+})
+
+app.get('/phone/:numbers' , (req, res)=>{
+    console.log(req.params);
+    res.send(`Ваш номер телефона: ${req.params.numbers}`)
+    
+})
+
+app.listen(PORT , ()=>{ // настраиваем сервер на чем он будет запущен
     console.log(`Сервер успешно запущен по адрессу: 127.0.0.1:${PORT}`);
     
 })
+
